afc28cc25293613658daa6292e941a400d370934
DemiënFeenstra <99064788@mydavinci.nl>
2022-01-12 11:46:14 +0100

    dictionary

--- MenMs.py
+++ MenMs.py
@@ -1,17 +1,50 @@
 import random
+
+aantalMenM = int(input("aantal kleuren MenMs"))
+
 kleuren = ["oranje", "blauw", "groen", "bruin"]
 zakMenM = []
 
-def function(kleuren):
-    aantalMenM = int(input("aantal kleuren MenMs"))
+def MenMtoevoegen(kleuren):
 
     for i in range(aantalMenM):
         kleurNum = random.randint(0, 3)
         zakMenM.append(kleuren[kleurNum])
+   
     return zakMenM
 
-function(kleuren)
+MenMtoevoegen(kleuren)
 print(zakMenM)
+
+# --------------------------------------------------dictionary--------------------------------------------------------------------
+
+DictionaryZak = {
+  "oranje": 0,
+  "blauw" : 0,
+  "groen" : 0,
+  "bruin" : 0
+}
+
+def MaakDictionary(aantalMenM,DictionaryZak):
+
+    for i in range(aantalMenM):
+        randomMenMkleur = random.choice(kleuren)    #  ---kiest random kleur---
+        DictionaryZak[randomMenMkleur] += 1         # ---voegt 1 m&m met kleur toe aan de DictionaryZak---
+   
+    return DictionaryZak
+
+MaakDictionary(aantalMenM,DictionaryZak)
+print(DictionaryZak)
+
+
+
+
+
+
+
+
+
+
 
 
 
